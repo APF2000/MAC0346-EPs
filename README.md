6e3861f0fe7f55cfe894332fa22f1aeb6f9dae74
APF2000 <arthurpfonseca@gmail.com>
2019-08-18 11:29:16 -0300

    Atributos

--- README.md
+++
@@ -1,2 +0,0 @@
-# MAC0346
-Aqui ficam os EPs e os trabalhos de MAC0346
---
+++ atributos.lua
@@ -0,0 +1,19 @@
+atributos = {
+
+  --[[
+      hit points, strength,   magic,
+      skill,      speed,      luck,
+      defense,    resistence, trait
+  ]]
+
+  HP,  str, mag,
+  skl, spd, lck,
+  def, res, trait
+
+}
+
+atributos.HP = 20
+print(atributos.HP)
+
+atributos.trait = 100
+print(atributos.trait)
