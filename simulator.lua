daf03b108f3482fa11659e5f0490bf15ef89610a
APF2000 <arthurpfonseca@gmail.com>
2019-08-21 23:19:19 -0300

    Codigo bem mais arejado agora. Extrai uma funcao do simulator.

--- simulator.lua
+++ simulator.lua
@@ -4,6 +4,19 @@ local combate = require "combate" --chamando módulo de combate
 
 local SIMULATOR = {}
 
+local function calculaCritical(unit_1, unit_2, weapons, ataque)
+  acerto = combate.acerto(unit_2, unit_1, weapons, math.random(1, 100), math.random(1, 100)) --flag de acerto do ataque
+  if acerto == 1 then--ataque acertou
+    critical = combate.critical(unit_2, unit_1, weapons, math.random(1, 100)) --flag para indicar se ataque é crítico
+  end
+  if(ataque == true) then
+    scenario_input.units[unit_1].hp = combate.attack(unit_2, unit_1, weapons, critical) --atualiza vida do defensor
+  end
+  else
+    scenario_input.units[unit_2].hp = combate.attack(unit_1, unit_2, weapons, critical)
+  end
+end
+
 function SIMULATOR.run(scenario_input)
   math.randomseed(scenario_input.seed) --inicializa gerador de números pseudoaleatórios com semente adequada
 
@@ -11,37 +24,30 @@ function SIMULATOR.run(scenario_input)
 
   local fight_number = table.getn(scenario_input0.fights) --dá número de lutas a serem realizadas neste cenário
   local count = 1 --contador
+
+
+
   while count <= fight_number do
     local unit_1 = scenario_input.units[scenario_input.fights[count][1]] --obtem unidade atacante
     local unit_2 = scenario_input.units[scenario_input.fights[count][2]] --obtem unidade defensora
 
     local double_attack = combate.double_attack(unit_1, unit_2, weapons) --flag para indicar se houve segundo ataque e de qual unidade
-    local acerto = combate.acerto(unit_1, unit_2, weapons, math.random(1, 100), math.random(1, 100)) --flag de acerto do ataque
-    if acerto == 1 then--ataque acertou
-      local critical = combate.critical(unit_1, unit_2, weapons, math.random(1, 100)) --flag para indicar se ataque é crítico
-    end
-    scenario_input.units[unit_2].hp = combate.attack(unit_1, unit_2, weapons, critical) --atualiza vida do defensor
+
+    calculaCritical(unit_1, unit_2, weapons, false)
+
 
     if scenario_input.units[unit_2].hp > 0 then --realiza contra-ataque
-      acerto = combate.acerto(unit_2, unit_1, weapons, math.random(1, 100), math.random(1, 100)) --flag de acerto do ataque
-      if acerto == 1 then--ataque acertou
-        critical = combate.critical(unit_2, unit_1, weapons, math.random(1, 100)) --flag para indicar se ataque é crítico
-      end
-      scenario_input.units[unit_1].hp = combate.attack(unit_2, unit_1, weapons, critical) --atualiza vida do defensor
+
+      calculaCritical(unit_1, unit_2, weapons, true)
+
 
       if scenario_input.units[unit_1].hp > 0 and double_attack > 0 then --realiza double attack
         if double_attack == 1 then --unidade 1 ataca novamente
-          acerto = combate.acerto(unit_1, unit_2, weapons, math.random(1, 100), math.random(1, 100)) --flag de acerto do ataque
-          if acerto == 1 then--ataque acertou
-            critical = combate.critical(unit_1, unit_2, weapons, math.random(1, 100)) --flag para indicar se ataque é crítico
-          end
-          scenario_input.units[unit_2].hp = combate.attack(unit_1, unit_2, weapons, critical) --atualiza vida do defensor
+
+          calculaCritical(unit_1, unit_2, weapons, false)
+
         else --unidade 1 ataca novamente
-          acerto = combate.acerto(unit_2, unit_1, weapons, math.random(1, 100), math.random(1, 100)) --flag de acerto do ataque
-          if acerto == 1 then--ataque acertou
-            critical = combate.critical(unit_2, unit_1, weapons, math.random(1, 100)) --flag para indicar se ataque é crítico
-          end
-          scenario_input.units[unit_1].hp = combate.attack(unit_2, unit_1, weapons, critical) --atualiza vida do defensor
+          calculaCritical(unit_1, unit_2, weapons, true)
         end
       end
     end
