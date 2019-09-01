e21ea903691085f6c018589fdbc0bfecfec6bf62
APF2000 <arthurpfonseca@gmail.com>
2019-09-01 01:29:31 -0300

    Progresso 0

--- EP2/main.lua
+++ EP2/main.lua
@@ -1,8 +1,16 @@
 local path = arg[2]
-path = string.format("maps/%s.lua", path)
+path = string.format("maps/maps/%s.lua", path)
+
+love.filesystem.setRequirePath(path)
+print(love.filesystem.getRequirePath())
 
 local chunck = love.filesystem.load(path)
 local MAP = chunck()
+
+
+function love.load()
+  --image = love.graphics.newImage(MAP.tilesets[1].image)
+end
 
 function love.draw()
 
@@ -12,5 +20,6 @@ function love.draw()
 
   love.graphics.print(MAP.version, 100, 100)
 
+  --love.graphics.draw(image, 0, 0)
 
 end
