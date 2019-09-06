0e2a0eeb8a5bc93941cd74038bbdb0a6d6060f14
APF2000 <arthurpfonseca@gmail.com>
2019-09-06 00:19:35 -0300

    Agora sem comentários inúteis e tudo em ordem.

--- EP2/main.lua
+++ EP2/main.lua
@@ -32,49 +32,14 @@ function love.load()
 
   spriteQuads = AUXLOADER.spriteQuads(MAP)
   sprites, cameras = AUXLOADER.objects(layers)
-  --camQuant = table.getn(cameras)
 
   love.graphics.setBackgroundColor(MAP.backgroundcolor)
 end
---[[]
-local current = 1
-local camQuant
-local camCurrent
-local Xprev, Yprev
-local Xctrl, Yctrl = 0, 0
-function love.update(dt)
-  camCurrent = cameras[tostring(current)]
-  Xprev, Yprev = camCurrent.x, camCurrent.y
 
-  if(Xnext == nil or camCurrent == nil) then
-    return
-  end
-
-  if (Xnext >= Xprev  and Xnext < Xctrl and Xprev > Xctrl)
-    or (Xprev < Xnext and Xnext < Xprev and Xnext > Xctrl)
-    or (Ynext >= Yprev and Ynext < Yctrl and Yprev > Yctrl)
-    or (Yprev < Ynext and Ynext < Yprev and Ynext > Yctrl)
-      then
-    current = (current % camQuant) + 1
-    camCurrent = cameras[tostring(current)]
-    Xnext = camCurrent.x
-    Ynext = camCurrent.y
-    Xctrl, Yctrl = Xprev, Yprev
-  else
-    local alpha = 2
-    local dx = (-Xprev + Xnext) / alpha
-    local dy = (-Yprev + Ynext) / alpha
-
-    Xctrl = Xctrl + dx
-    Yctrl = Yctrl + dy
-  end
-end]]
 
 local function render()
   local z
   local w, h = MAP.width, MAP.height
-
-    --love.graphics.translate(Xctrl, Yctrl)
 
     for _, layer in ipairs(layers) do
 
