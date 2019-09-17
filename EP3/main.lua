41b80d71801afe1c3cb97b51068bb3d1ca271d40
APF2000 <arthurpfonseca@gmail.com>
2019-09-17 19:44:33 -0300

    Orientacao a objetos resolvida

--- EP3/main.lua
+++ EP3/main.lua
@@ -1,11 +1,11 @@
-local Vec = require "common/vec"
-local v2 = require "common/vec"
---local v1 = Vec_init(1,1)
+local newVec = require "common/vec"
+local v2 = newVec()
+local v1 = newVec()
 
---v1:_init(1, 1)
+v1:_init(1, 1)
 v2:_init(1, 2)
 
---print(v1:__tostring())
+print(v1:__tostring())
 print(v2:__tostring())
 
-print(v2:dot(v2))
+print(v2:dot(v1))
