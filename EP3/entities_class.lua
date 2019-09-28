799aa110b3a6521dcbd0d486818cf795a32ede38
AndrésEMC <a.cologne9@gmail.com>
2019-09-28 18:56:04 -0300

    Colocando classe alterada com funções de desenho.

--- EP3/entities_class.lua
+++ EP3/entities_class.lua
@@ -50,7 +50,7 @@ local function requireEntObj(name)
   return loaded()
 end
 
---- According to the name, delete the needless fields and set used fields
+--- According to the name, delete the needless fields and set used fields:
 function Entity:set(name)
   local path = "entity/" .. name
   local entity = require(path)
@@ -69,5 +69,61 @@ function Entity:set(name)
   self.field = entity.field
   self.charge = entity.charge
 end
+
+--- Draws entity according to their properties:
+function Entity:draw(self)
+  local hasfield = 0 --flag to tell if entity has field property
+  local hasbody = 0 --flag to tell if entity has body property
+
+  if self.position then --Entity has position
+    local x,y = self.position.point:get() --store entitys position
+
+    if self.field then --Entity has field property
+      hasfield = 1
+      -- Defining color of field:
+      if self.field.strength < 0 then
+        love.graphics.setColor(1, 0, 0) --red
+      elseif self.field.strength > 0 then
+        love.graphics.setColor(0, 0, 1) --blue
+      else
+        love.graphics.setColor(0, 1, 0) --green
+      end
+      -- Drawing field:
+      love.graphics.circle('line', x, y, math.abs(self.field.strength))
+    end
+
+    if self.body then --Entity has body property
+      hasbody = 1
+      -- Defining color of field:
+      if hasfield == 0 then --entity does't have field property
+        love.graphics.setColor(0, 1, 0) --green
+      end
+      -- Drawing field:
+      love.graphics.circle('fill', x, y, math.abs(self.body.size))
+    end
+
+    if self.charge then --Entity has charge property
+      -- Defining color of charge:
+      if self.charge.strength < 0 then
+        love.graphics.setColor(1, 0, 0) --red
+      elseif self.charge.strength > 0 then
+        love.graphics.setColor(0, 0, 1) --blue
+      else
+        love.graphics.setColor(0, 1, 0) --green
+      end
+      -- Drawing field:
+      love.graphics.circle('fill', x+8, y, 4)
+      love.graphics.circle('fill', x-8, y, 4)
+    end
+
+    if hasfield == 0 and hasbody == 0 then --draw default circle
+      -- Defining color of default circle:
+      love.graphics.setColor(0, 1, 0) --green
+      -- Drawing default circle:
+      love.graphics.circle('line', x, y, 8)
+    end
+  end
+end
+
 
 return Entity
